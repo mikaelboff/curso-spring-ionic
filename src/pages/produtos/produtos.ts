8f33d7ae9e5e6fbc7eb59892c18a4a80c56f61db
Mikael Boff <mikaelboff1@gmail.com>
2018-10-04 21:57:39 -0300

    Carregando imagens dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -26,4 +27,11 @@ export class ProdutosPage {
       }, falha => { });
   }
 
+  loadImageUrls() {
+    this.itens.forEach(item => this.service.getSmallImageFromBucket(item.id).subscribe(sucesso => {
+      item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+    }, falha => {
+
+    }))
+  }
 }
--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../../config/api.config";
 import { Observable } from "rxjs/Rx";
-import { ProdutoDTO } from "../../models/produto.dto";
 
 @Injectable()
 export class ProdutoService {
-    constructor(public httpClient: HttpClient) {
-
-    }
+    constructor(public httpClient: HttpClient) { }
 
     findAllByCategoria(idCategoria: number) {
         return this.httpClient.get(`${API_CONFIG.baseUrl}/produtos?categorias=${idCategoria}`);
+    }
+
+    getSmallImageFromBucket(idProduto: string): Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${idProduto}-small.jpg`;
+        return this.httpClient.get(url, { responseType: 'blob' });
     }
 }
