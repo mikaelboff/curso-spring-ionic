af8f5f1da6f773ddad734ae7a76847fe478b941c
Mikael Boff <mikaelboff1@gmail.com>
2018-10-07 23:15:21 -0300

    Buscando endereços do banco de dados

--- src/pages/escolher-endereco/escolher-endereco.ts
+++ src/pages/escolher-endereco/escolher-endereco.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
+import { StorageService } from '../../services/storage.service';
 
 @IonicPage()
 @Component({
@@ -13,10 +15,27 @@ export class EscolherEnderecoPage {
 
   constructor(
     public navCtrl: NavController,
-    public navParams: NavParams) {
-  }
+    public navParams: NavParams,
+    private clienteService: ClienteService,
+    private storage: StorageService
+  ) { }
 
   ionViewDidLoad() {
+    let localUser = this.storage.getLocalUser();
+    if (localUser && localUser.email) {
+      this.clienteService.findByEmail(localUser.email)
+        .subscribe(response => {
+          this.items = response;
+          this.getImageIfExists();
+        },
+          error => {
+            if (error.status == 403) {
+              this.navCtrl.setRoot("HomePage");
+            }
+          });
+    } else {
+      this.navCtrl.setRoot("HomePage");
+    }
   }
 
 }
