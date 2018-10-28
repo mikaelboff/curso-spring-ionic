1c82ad49548b6ab00b2a8f4d94ffe0248655eea5
Mikael Boff <mikaelboff1@gmail.com>
2018-10-27 22:00:45 -0200

    Ajustado formato da imagem na seleção de imagem camera ou galeria na página de perfil

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -60,7 +60,7 @@ export class ProfilePage {
 
     const options: CameraOptions = {
       quality: 100,
-      destinationType: this.camera.DestinationType.FILE_URI,
+      destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.PNG,
       mediaType: this.camera.MediaType.PICTURE
     }
